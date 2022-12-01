e386c2aa9a344059ae53c9106b6f84c491b237d2
zuu <i.tis.to.be.a.fan@gmail.com>
2022-12-01 11:45:16 -0800

    Continued progress
    Started to add prefabs. Started to add collisions to game objects. Finished update to ruby's script (RubysController)

--- RubysAdventureHosang/Assets/Scripts/RubyController.cs
+++ RubysAdventureHosang/Assets/Scripts/RubyController.cs
@@ -3,28 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RubyController : MonoBehaviour
-{
+{ 
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //Creating the two varibles that use the pre built axes
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+         horizontal = Input.GetAxis("Horizontal");
+         vertical = Input.GetAxis("Vertical");
 
+    }
+
+    private void FixedUpdate()
+    {
         //create the movement vector
-        Vector2 position = transform.position;
+        Vector2 position = rigidbody2d.position;
 
         //changing the x and y position 
         position.x = position.x + 5.0f * horizontal * Time.deltaTime;
         position.y = position.y + 5.0f * vertical * Time.deltaTime;
 
         //set the update position
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
     }
 }
