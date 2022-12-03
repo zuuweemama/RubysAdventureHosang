5fa46b4e1eb9f6b6e90d2b7089681380003b0d19
zuu <i.tis.to.be.a.fan@gmail.com>
2022-12-03 12:47:09 -0800

    Almost done
    finished up to World Interactions - damage zones and enemies. Started Sprite animation, got to step 6.

---
+++ RubysAdventureHosang/Assets/Scripts/DamageZone.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageZone : MonoBehaviour
+{
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        RubyController controller = other.GetComponent<RubyController>();
+
+        if (controller != null)
+        {
+            controller.ChangeHealth(-1);
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- RubysAdventureHosang/Assets/Scripts/RubyController.cs
+++ RubysAdventureHosang/Assets/Scripts/RubyController.cs
@@ -3,7 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RubyController : MonoBehaviour
-{ 
+{
+    public float speed = 5.0f;
+
+    public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
+
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
+    bool isInvincible;
+    float invincibleTimer;
+
+
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -13,6 +26,7 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -22,6 +36,16 @@ public class RubyController : MonoBehaviour
          horizontal = Input.GetAxis("Horizontal");
          vertical = Input.GetAxis("Vertical");
 
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+
+
+
     }
 
     private void FixedUpdate()
@@ -30,10 +54,27 @@ public class RubyController : MonoBehaviour
         Vector2 position = rigidbody2d.position;
 
         //changing the x and y position 
-        position.x = position.x + 5.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 5.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         //set the update position
         rigidbody2d.MovePosition(position);
     }
+
+    public void ChangeHealth(int amount)
+    {
+
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
 }
